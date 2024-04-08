ddf7f1befba15d10f9d65c3981d26c196d734348
Jacob <jcmuffin18@gmail.com>
2024-04-08 13:18:50 +0200

    work on ex 2

--- 4_hardhat/scripts/2_ex_deploy.js
+++ 4_hardhat/scripts/2_ex_deploy.js
@@ -10,9 +10,9 @@
 // Text below taken from official hardhat template:
 
 // We require the Hardhat Runtime Environment explicitly here. This is optional
-// but useful for running the script in a standalone fashion through `node <script>`.
+// but useful for running the script in a standalone fashion through //* `node <script>`.
 //
-// You can also run a script with `npx hardhat run <script>`. If you do that, Hardhat
+// You can also run a script with //*  `npx hardhat run <script>` . If you do that, Hardhat
 // will compile your contracts, add the Hardhat Runtime Environment's members to the
 // global scope, and execute the script.
 
