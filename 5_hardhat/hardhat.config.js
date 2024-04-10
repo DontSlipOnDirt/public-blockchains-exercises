398c437795577692fda43ae004b6bde07f593218
Jacob <jcmuffin18@gmail.com>
2024-04-10 12:58:28 +0200

    work on ex 6

--- 5_hardhat/hardhat.config.js
+++ 5_hardhat/hardhat.config.js
@@ -1,6 +1,13 @@
 require("@nomicfoundation/hardhat-toolbox");
+const dotenv = require('dotenv').config('C:/Users/jcmuf/Desktop/Public Blockchain/public-blockchains-exercises/.env');
 
 /** @type import('hardhat/config').HardhatUserConfig */
 module.exports = {
   solidity: "0.8.24",
+  networks: {
+    sepolia: {
+      url: dotenv.ALCHEMY_SEPOLIA_API_URL,
+      accounts: [dotenv.METAMASK_1_PRIVATE_KEY,]
+    }
+  }
 };
